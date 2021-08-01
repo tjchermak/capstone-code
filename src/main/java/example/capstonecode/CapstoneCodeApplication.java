469a5fd2bbcdbc2c216123c43a85efdfbc7d90ea
tjchermak <tjchermak@gmail.com>
2021-08-01 10:38:24 -0400

    Update to code for Hello World

--- src/main/java/example/capstonecode/CapstoneCodeApplication.java
+++ src/main/java/example/capstonecode/CapstoneCodeApplication.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CapstoneCodeApplication {
 
+@RequestMapping("/")
+  public String home() {
+    return "Hello Docker World";
+  }
+
 	public static void main(String[] args) {
 		SpringApplication.run(CapstoneCodeApplication.class, args);
 	}
