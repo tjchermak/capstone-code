332981786d8fa7184d610706aa580ccd2185f00d
tjchermak <tjchermak@gmail.com>
2021-08-01 10:25:52 -0400

    new files to repo

---
+++ src/main/java/example/capstonecode/CapstoneCodeApplication.java
@@ -0,0 +1,13 @@
+package example.capstonecode;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapstoneCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CapstoneCodeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/example/capstonecode/CapstoneCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package example.capstonecode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
